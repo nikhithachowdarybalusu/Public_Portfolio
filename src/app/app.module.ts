b74575cdbb3c41cd58167dade7382f60cc4c34c5
nikhithachowdarybalusu <nikhithachowdarybalusu@gmail.com>
2025-02-25 18:33:04 -0600

    Services structure created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ResumeUploadComponent } from './components/navbar/resume-upload/resume-upload.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { ProfessionalExperienceComponent } from './pages/professional-experience/professional-experience.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ResumeComponent } from './pages/resume/resume.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ResumeUploadComponent
+    ResumeUploadComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ProfessionalExperienceComponent,
+    ContactComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule,
