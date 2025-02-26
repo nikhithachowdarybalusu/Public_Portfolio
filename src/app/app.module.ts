1ceac584614a4efb9d2e538d5051e2fbc40c08f3
nikhithachowdarybalusu <nikhithachowdarybalusu@gmail.com>
2025-02-25 18:33:25 -0600

    Services structure created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { ResumeUploadComponent } from './components/navbar/resume-upload/resume-upload.component';
+import { ResumeUploadComponent } from './components/resume-upload/resume-upload.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
