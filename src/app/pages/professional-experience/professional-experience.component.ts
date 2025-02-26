0c324e97611e2222d1229d32bd3d747c1b4a9bed
nikhithachowdarybalusu <nikhithachowdarybalusu@gmail.com>
2025-02-25 18:31:24 -0600

    Component structure created

---
+++ src/app/pages/professional-experience/professional-experience.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-professional-experience',
+  templateUrl: './professional-experience.component.html',
+  styleUrls: ['./professional-experience.component.scss']
+})
+export class ProfessionalExperienceComponent {
+
+}
