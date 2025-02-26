4a1283fa5b3822883b0873043e35fbf54bf52565
nikhithachowdarybalusu <nikhithachowdarybalusu@gmail.com>
2025-02-25 18:26:22 -0600

    Added base href

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { ResumeUploadComponent } from './resume-upload/resume-upload.component';
+import { ResumeUploadComponent } from './components/navbar/resume-upload/resume-upload.component';
 
 @NgModule({
   declarations: [
